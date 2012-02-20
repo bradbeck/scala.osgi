0064e918bf685f2170569c6da36be98432fed6aa
Brad Beck <bradley.beck@gmail.com>
2012-02-19 18:17:30 -0600

    added argument support and annotations

--- README.txt
+++ README.txt
@@ -6,4 +6,5 @@ karaf@root> install -s wrap:mvn:org.scala-lang/scala-library/2.9.1
 karaf@root> install -s mvn:com.example/scala.osgi.greeter/1.0.0-SNAPSHOT
 karaf@root> install -s mvn:com.example/scala.osgi.greeter.impl/1.0.0-SNAPSHOT
 karaf@root> install -s mvn:com.example/scala.osgi.greeter.cmd/1.0.0-SNAPSHOT
-karaf@root> greeter:greet
+karaf@root> greeter:greet Fred
+Hello Fred
--- scala.osgi.greeter.cmd/src/main/scala/com/example/scala/osgi/greeter/impl/GreeterCommand.scala
+++ scala.osgi.greeter.cmd/src/main/scala/com/example/scala/osgi/greeter/impl/GreeterCommand.scala
@@ -1,16 +1,23 @@
 package com.example.scala.osgi.greeter.impl
+
+import org.apache.felix.gogo.commands.Argument
+import org.apache.felix.gogo.commands.Command
 import org.apache.karaf.shell.console.OsgiCommandSupport
 import com.example.scala.osgi.greeter.Greeter
 
+@Command(scope = "greeter", name = "greet", description="Says hello")
 class GreeterCommand extends OsgiCommandSupport {
   var greeter: Greeter = null
 
+  @Argument(index = 0, name = "name", description = "the name to greet", required = true, multiValued = false)
+  var name: String = null
+
   override
   def doExecute(): Object  = {
-	  greeter greet "Fred"
+	  greeter greet name
   }
   
-  def setGreeter(g:Greeter) = {
+  def setGreeter(g: Greeter) = {
     greeter = g
   }
 }
